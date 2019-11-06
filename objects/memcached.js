509633dd910c4cfd72d2408a74a80cb3015f76c4
Mike Spadafora <miikekm@gmail.com>
2019-11-06 15:34:35 -0500

    fixed something

--- objects/memcached.js
+++ objects/memcached.js
@@ -12,7 +12,7 @@ class Memcached{
         //     }
         // });
 
-        this.memcahced = memjs.Client.create('localhost:11211')
+        this.memcached = memjs.Client.create('localhost:11211')
     }
 
     async cacheQueryResult(query, result) {
@@ -23,7 +23,7 @@ class Memcached{
         let result = this.memcached.get(query, function(err, value) {
             console.log(value.toString())
         }) 
-        
+
         return result
     }   
 }
