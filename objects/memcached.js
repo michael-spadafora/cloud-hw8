d8de52e1c85faf8b3c3ed5502198fbe0b04c1c9d
Mike Spadafora <miikekm@gmail.com>
2019-11-06 15:40:39 -0500

    fixed something

--- objects/memcached.js
+++ objects/memcached.js
@@ -1,30 +1,25 @@
 var Memcachedd = require('memcached');
 const util = require('util');
 
-var memjs = require('memjs')
 
 class Memcached{
     constructor(){
-        // this.memcached = new Memcachedd();
-        // this.memcached.connect( 'localhost:11211', function( err, conn ){
-        //     if( err ) {
-        //        console.log( conn.server );
-        //     }
-        // });
-
-        this.memcached = memjs.Client.create('localhost:11211')
+        this.memcached = new Memcachedd();
+        this.memcached.connect( 'localhost:11211', function( err, conn ){
+            if( err ) {
+               console.log( conn.server );
+            }
+        });
     }
 
     async cacheQueryResult(query, result) {
-        await this.memcached.set(query, result)   
+        await memcached.set(query, result, 10, function (err) { /* stuff */ });
     }
 
     async getQueryResult(query) {
-        let result = this.memcached.get(query, function(err, value) {
-            if (value)
-            console.log(value.toString())
-        }) 
-
+        let get = util.promisify(this.memcached.set)
+        let result = await get(query)
+        console.log(result)
         return result
     }   
 }
