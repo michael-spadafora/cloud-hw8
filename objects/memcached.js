48d5a8e3d2f0c5f7625cd72efd05d01efa708f96
Mike Spadafora <miikekm@gmail.com>
2019-11-06 13:40:50 -0500

    fixed something

--- objects/memcached.js
+++ objects/memcached.js
@@ -3,7 +3,7 @@ var Memcachedd = require('memcached');
 class Memcached{
     constructor(){
         this.memcached = new Memcachedd();
-        memcached.connect( 'localhost:11211', function( err, conn ){
+        this.memcached.connect( 'localhost:11211', function( err, conn ){
             if( err ) {
                console.log( conn.server );
             }
