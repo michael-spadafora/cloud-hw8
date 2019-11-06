2fd85da15d21c638d8bb7845abec7848493db117
Mike Spadafora <miikekm@gmail.com>
2019-11-06 14:31:34 -0500

    fixed something

--- objects/mysql.js
+++ objects/mysql.js
@@ -13,7 +13,7 @@ class MySQL {
 
     async getInformation(club, pos) {
         let query1 = "SELECT Player, A from test WHERE Club=`" + club + "` AND POS=`" + pos + "` ORDER BY A DESC, GS DESC, NAME DESC"
-        let query2 = "SELECT AVG(A) from test WHERE Club=" + club + " AND POS=" + pos 
+        let query2 = "SELECT AVG(A) from test WHERE Club=`" + club + "` AND POS=`" + pos +'`'
 
         let player= await this.connection.query(query1)
         let avg_assists = await this.connection.query(query2)
